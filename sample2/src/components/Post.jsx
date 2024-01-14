53954c9dd3691ef3d243e2181ae36997c16fd1e5
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-14 18:12:48 +0900

    1

--- sample2/src/components/Post.jsx
+++ sample2/src/components/Post.jsx
@@ -5,12 +5,6 @@ import classes2 from './test2.module.css'
 function Post(props) {
     return (
     <div className={classes.post}>
-        <h1>이건 제목</h1>
-        <ul>
-            <li>1번</li>
-            <li>2번</li>
-        </ul>
-        <h2 className={classes2.sample}>이건 내용</h2>
         <p className={classes.author}>{props.author}</p>
         <p className={classes.text}>{props.body}</p>
     </div>
