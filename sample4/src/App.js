ee8369d64ef2489819196c5b221bfc98b5a1da83
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-14 19:23:41 +0900

    1

--- sample4/src/App.js
+++ sample4/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Nav, Navbar, Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="Navbar" bg="dark" variant="dark">
+        <Navbar.Collapse className="d-flex justify-content-between">
+          <Navbar.Brand href="#home" className="m-2">SUSINSA
+          <input className="m-2"></input>
+          </Navbar.Brand>
+          <Nav className="m-2">
+            <Nav.Link href="#mypage">마이페이지</Nav.Link>
+            <Nav.Link href="#features">최근 본 상품</Nav.Link>
+            <Nav.Link href="#pricing">좋아요</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
