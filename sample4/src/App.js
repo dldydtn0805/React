f067f53c5549c709b7c700a61ee586ecb40b308d
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-15 23:54:03 +0900

    1

--- sample4/src/App.js
+++ sample4/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Nav, Navbar, Container} from 'react-bootstrap';
-
+import { useState } from 'react';
+import {Button, Nav, Navbar, Container, Row, Col} from 'react-bootstrap';
+import 작명 from './img/bg.png'
+import data from './data.js'
 function App() {
+
+  let [shoes] = useState(data)
+  
   return (
     <div className="App">
       <Navbar className="Navbar" bg="dark" variant="dark">
@@ -18,8 +23,30 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <div className='main-bg'></div>
+      <Container>
+      <Row>
+        {
+          shoes.map((a, i) => {
+            return(
+              <Card i = {i} shoes = {shoes[i]}></Card>
+            )
+          })
+        }
+      </Row>
+    </Container>
     </div>
   );
 }
 
+
+function Card(props) {
+  return (
+      <Col sm>
+          <img src={`https://codingapple1.github.io/shop/shoes${props.i+1}.jpg`} width="80%"></img>
+          <h4>{props.shoes.title}</h4>
+          <h4>{props.shoes.price}</h4>
+      </Col>
+  )
+}
 export default App;
