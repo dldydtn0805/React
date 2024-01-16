a444195fe85582c4d5e486bb26ebed8bd3834f10
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-16 23:52:39 +0900

    1

--- sample4/src/App.js
+++ sample4/src/App.js
@@ -5,9 +5,10 @@ import { useState } from 'react';
 import {Button, Nav, Navbar, Container, Row, Col} from 'react-bootstrap';
 import 작명 from './img/bg.png'
 import data from './data.js'
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './pages/Detail.js'
 function App() {
 
-  let [shoes] = useState(data)
   
   return (
     <div className="App">
@@ -17,28 +18,43 @@ function App() {
           <input className="m-2"></input>
           </Navbar.Brand>
           <Nav className="m-2">
-            <Nav.Link href="#mypage">마이페이지</Nav.Link>
-            <Nav.Link href="#features">최근 본 상품</Nav.Link>
+            <Nav.Link href="/">홈</Nav.Link>
+            <Nav.Link href="/detail">상세 페이지 </Nav.Link>
             <Nav.Link href="#pricing">좋아요</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <div className='main-bg'></div>
-      <Container>
-      <Row>
-        {
-          shoes.map((a, i) => {
-            return(
-              <Card i = {i} shoes = {shoes[i]}></Card>
-            )
-          })
-        }
-      </Row>
-    </Container>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+      
+      <Routes>
+        <Route path="/" element={<Main/>}></Route>
+        <Route path="/detail" element={<Detail/>}/>
+      </Routes>
     </div>
   );
 }
 
+function Main() {
+  let [shoes] = useState(data)
+  
+  return (
+    <div>
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          {
+            shoes.map((a, i) => {
+              return(
+                <Card i = {i} shoes = {shoes[i]}></Card>
+              )
+            })
+          }
+        </Row>
+      </Container>
+    </div>
+  )
+}
 
 function Card(props) {
   return (
