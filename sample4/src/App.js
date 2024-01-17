1bf2daab83577b6aba6dfb452cced4e6bf1b2ec5
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-17 21:00:29 +0900

    1

--- sample4/src/App.js
+++ sample4/src/App.js
@@ -8,7 +8,8 @@ import data from './data.js'
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 import Detail from './pages/Detail.js'
 function App() {
-
+  let [shoes] = useState(data)
+  let navigate = useNavigate();
   
   return (
     <div className="App">
@@ -18,23 +19,48 @@ function App() {
           <input className="m-2"></input>
           </Navbar.Brand>
           <Nav className="m-2">
-            <Nav.Link href="/">홈</Nav.Link>
-            <Nav.Link href="/detail">상세 페이지 </Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>홈</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}} >상세 페이지 </Nav.Link>
             <Nav.Link href="#pricing">좋아요</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
-      
       <Routes>
         <Route path="/" element={<Main/>}></Route>
-        <Route path="/detail" element={<Detail/>}/>
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>}></Route>
+          <Route path="location" element={<div>위치정보임</div>}></Route>
+        </Route>
+        <Route path="/event" element={<Event></Event>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path="two" element={<div>생일기념 쿠폰 받기</div>}></Route>
+        </Route>
+        <Route path="/about/member" element={<About />}/>
+        <Route path="/about/location" element={<About />}/>
+        <Route path="*" element={<div>없는페이지요</div>}></Route>
       </Routes>
     </div>
   );
 }
 
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
 function Main() {
   let [shoes] = useState(data)
   
--- sample4/src/pages/Detail.js
+++ sample4/src/pages/Detail.js
@@ -1,17 +1,31 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+  let {id} = useParams();
+  console.log(props.shoes[0])
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      {
+        id == 1 | id == 2 | id == 3
+        ?
+        <div className="row">
+          <div className="col-md-6">
+            <img src= {`https://codingapple1.github.io/shop/shoes${id}.jpg`} width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{props.shoes.map((x)=>{
+              if (x.id == id-1) {
+                return (JSON.stringify(x))
+              }
+              })}</h4>
+            
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
         </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button> 
-        </div>
-      </div>
+        :
+        <div>페이지가 이상해요</div> 
+      }
+      
     </div> 
   )
 }
