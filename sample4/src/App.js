17fdd79311cdbcd38b3519555f341f018a50a1e8
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-19 13:08:12 +0900

    feat : 새로운 기능 추가

--- sample4/src/App.js
+++ sample4/src/App.js
@@ -1,62 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import {Button, Nav, Navbar, Container, Row, Col} from 'react-bootstrap';
-import 작명 from './img/bg.png'
-import data from './data.js'
-import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
-import Detail from './pages/Detail.js'
-import axios from 'axios';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
+import 작명 from "./img/bg.png";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./pages/Detail.js";
+import axios from "axios";
 function App() {
-  let [shoes, 슈즈변경] = useState(data)
+  let [shoes, 슈즈변경] = useState(data);
   let navigate = useNavigate();
-  
+  let [버튼클릭수, 버튼클릭수변경] = useState(0);
+  let [로딩상태, 로딩상태변경] = useState(false)
   return (
     <div className="App">
       <Navbar className="Navbar" bg="dark" variant="dark">
         <Navbar.Collapse className="d-flex justify-content-between">
-          <Navbar.Brand href="/" className="m-2">SUSINSA
-          <input className="m-2"></input>
+          <Navbar.Brand href="/" className="m-2">
+            SUSINSA
+            <input className="m-2"></input>
           </Navbar.Brand>
           <Nav className="m-2">
-            <Nav.Link onClick={()=>{navigate('/')}}>홈</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/detail')}} >상세 페이지 </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              홈
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              상세 페이지{" "}
+            </Nav.Link>
             <Nav.Link href="#pricing">좋아요</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      
+
       <Routes>
-        <Route path="/" element={<Main shoes = {shoes}/>}></Route>
-        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
+        <Route path="/" element={<Main shoes={shoes} 버튼클릭수변경={버튼클릭수변경} 버튼클릭수={버튼클릭수} sh />}></Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>멤버임</div>}></Route>
           <Route path="location" element={<div>위치정보임</div>}></Route>
         </Route>
         <Route path="/event" element={<Event></Event>}>
-          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route
+            path="one"
+            element={<div>첫 주문시 양배추즙 서비스</div>}
+          ></Route>
           <Route path="two" element={<div>생일기념 쿠폰 받기</div>}></Route>
         </Route>
-        <Route path="/about/member" element={<About />}/>
-        <Route path="/about/location" element={<About />}/>
+        <Route path="/about/member" element={<About />} />
+        <Route path="/about/location" element={<About />} />
         <Route path="*" element={<div>없는페이지요</div>}></Route>
       </Routes>
-      <button onClick={()=> {
-        axios.get('https://codingapple1.github.io/shop/data2.json')
-        .then(결과=>{
-          let copy = [...shoes, ...결과.data];
-          console.log(copy)
-          슈즈변경(copy)
-        })
-        .catch(() => {
-          console.log('실패함ㅅㄱ')
-        })
-      }}>아무버튼</button>
+      {
+        로딩상태 == true
+        ? <div className="로딩중">로딩중</div>
+        : null
+      }
+
     </div>
   );
 }
-
 
 function Event() {
   return (
@@ -64,7 +75,7 @@ function Event() {
       <h4>오늘의 이벤트</h4>
       <Outlet></Outlet>
     </div>
-  )
+  );
 }
 function About() {
   return (
@@ -72,35 +83,70 @@ function About() {
       <h4>회사정보임</h4>
       <Outlet></Outlet>
     </div>
-  )
+  );
 }
 function Main(props) {
-  
   return (
     <div>
-      <div className='main-bg'></div>
+      <div className="main-bg"></div>
       <Container>
         <Row>
-          {
-            props.shoes.map((a, i) => {
-              return(
-                <Card i = {i} shoes = {a}></Card>
-              )
-            })
-          }
+          {props.shoes.map((a, i) => {
+            return <Card i={i} shoes={a}></Card>;
+          })}
         </Row>
       </Container>
+      <button
+        onClick={() => {
+          버튼클릭수변경(버튼클릭수 + 1); 
+          if (버튼클릭수 == 1) {
+            axios
+              .get("https://codingapple1.github.io/shop/data2.json")
+              .then((결과) => {
+                let copy = [...shoes, ...결과.data];
+                console.log(copy);
+                슈즈변경(copy);
+                로딩상태변경(false)
+              })
+              .catch(() => {
+                console.log("버튼클릭 1차 실패함ㅅㄱ");
+                로딩상태변경(false)
+              });
+          } else if (버튼클릭수 == 2) {
+            axios
+              .get("https://codingapple1.github.io/shop/data3.json")
+              .then((결과) => {
+                let copy = [...shoes, ...결과.data];
+                console.log(copy);
+                슈즈변경(copy);
+                로딩상태변경(false)
+              })
+              .catch(() => {
+                console.log("버튼클릭 2차 실패함ㅅㄱ");
+                로딩상태변경(false)
+              });
+          } else {
+            console.log("상품 더 없음");
+            로딩상태변경(false)
+          }
+        }}
+      >
+        아무버튼
+      </button>
     </div>
-  )
+  );
 }
 
 function Card(props) {
   return (
-      <Col sm>
-          <img src={`https://codingapple1.github.io/shop/shoes${props.i+1}.jpg`} width="80%"></img>
-          <h4>{props.shoes.title}</h4>
-          <h4>{props.shoes.price}</h4>
-      </Col>
-  )
+    <Col sm>
+      <img
+        src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`}
+        width="80%"
+      ></img>
+      <h4>{props.shoes.title}</h4>
+      <h4>{props.shoes.price}</h4>
+    </Col>
+  );
 }
 export default App;
--- sample4/src/pages/Detail.js
+++ sample4/src/pages/Detail.js
@@ -1,23 +1,25 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import './Detail.css';
+import { Nav } from "react-bootstrap";
+import "./Detail.css";
 
 function Detail(props) {
   let { id } = useParams();
   let [count, setCount] = useState(0);
   let [discount, setDiscount] = useState(true);
   let [숫자경고, 숫자경고변경] = useState(false);
-  let [입력값, 입력값변경] = useState('');
+  let [입력값, 입력값변경] = useState("");
+  let [탭, 탭변경] = useState(2);
+
   useEffect(() => {
     let a = setTimeout(() => {
       setDiscount(false);
     }, 2000);
 
     if (isNaN(입력값)) {
-      숫자경고변경(true)
-    }
-    else {
-      숫자경고변경(false)
+      숫자경고변경(true);
+    } else {
+      숫자경고변경(false);
     }
     return () => {
       console.log(1);
@@ -30,39 +32,30 @@ function Detail(props) {
       {discount == true ? (
         <div className="alert alert-warning">2초 이내 구매시 할인</div>
       ) : null}
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        버튼
-      </button>
-      {count}
-      {id in [0,1,2] ? (
+      {id in [0, 1, 2] ? (
         <div className="row">
           <div className="col-md-6">
             <img
-              src={`https://codingapple1.github.io/shop/shoes${Number(id)+1}.jpg`}
+              src={`https://codingapple1.github.io/shop/shoes${
+                Number(id) + 1
+              }.jpg`}
               width="100%"
               alt=""
             />
           </div>
           <div></div>
-          {
-            숫자경고 == true
-            ? <div className="alert1">
-              경고: 숫자만 입력하세요
-              </div>
-            : null
-          }
+          {숫자경고 == true ? (
+            <div className="alert1">경고: 숫자만 입력하세요</div>
+          ) : null}
           <div></div>
-          <input className="input1"
+          <input
+            className="input1"
             onChange={(e) => {
-              입력값변경(e.target.value)
+              입력값변경(e.target.value);
             }}
-            />
-       
-            <div></div>
+          />
+
+          <div></div>
           <div className="col-md-6">
             <h4 className="pt-5">찾은 상품 : {filteredShoes[0].title}</h4>
             <p>{filteredShoes[0].content}</p>
@@ -70,6 +63,18 @@ function Detail(props) {
 
             <button className="btn btn-danger">주문하기</button>
           </div>
+          <Nav variant="tabs" defaultActiveKey="link1">
+            <Nav.Item>
+              <Nav.Link eventKey="link0" onClick={()=>{탭변경(0)}}>버튼0</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link1" onClick={()=>{탭변경(1)}}>버튼1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link2" onClick={()=>{탭변경(2)}}>버튼2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <TapContent 탭={탭}></TapContent>
         </div>
       ) : (
         <div>페이지가 이상해요</div>
@@ -78,4 +83,15 @@ function Detail(props) {
   );
 }
 
+function TapContent(props) {
+  if (props.탭 == 0) {
+    return <div>내용0</div>;
+  }
+  if (props.탭 == 1) {
+    return <div>내용1</div>;
+  }
+  if (props.탭 == 2) {
+    return <div>내용2</div>;
+  }
+}
 export default Detail;
