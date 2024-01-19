09076a0bd7e05dedac80de5cbf135730f2aed456
dldydtn0805 <ssafysoo95@gmail.com>
2024-01-19 17:24:29 +0900

    1

--- sample4/src/App.js
+++ sample4/src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useEffect, useState, createContext } from "react";
 import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 import 작명 from "./img/bg.png";
 import data from "./data.js";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./pages/Detail.js";
 import axios from "axios";
+
+export let Context1 = createContext();
+
 function App() {
   let [shoes, 슈즈변경] = useState(data);
+  let [재고, 재고변경] = useState([10, 11, 12]);
   let navigate = useNavigate();
   let [버튼클릭수, 버튼클릭수변경] = useState(0);
-  let [로딩상태, 로딩상태변경] = useState(false)
+  let [로딩상태, 로딩상태변경] = useState(false);
   return (
     <div className="App">
       <Navbar className="Navbar" bg="dark" variant="dark">
@@ -42,8 +46,78 @@ function App() {
       </Navbar>
 
       <Routes>
-        <Route path="/" element={<Main shoes={shoes} 버튼클릭수변경={버튼클릭수변경} 버튼클릭수={버튼클릭수} sh />}></Route>
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map((a, i) => {
+                    return <Card i={i} shoes={a}></Card>;
+                  })}
+                </Row>
+              </Container>
+              {로딩상태 == true ? <div>로딩중</div> : null}
+              <button
+                onClick={() => {
+                  버튼클릭수변경(버튼클릭수 + 1);
+                  로딩상태변경(true);
+                  if (버튼클릭수 == 0) {
+                    axios
+                      .get("https://codingapple1.github.io/shop/data2.json")
+                      .then((결과) => {
+                        let copy = [...shoes, ...결과.data];
+                        console.log(copy);
+                        슈즈변경(copy);
+                        setTimeout(() => {
+                          로딩상태변경(false);
+                        }, 500);
+                      })
+                      .catch(() => {
+                        console.log("버튼클릭 1차 실패함ㅅㄱ");
+                        setTimeout(() => {
+                          로딩상태변경(false);
+                        }, 500);
+                      });
+                  } else if (버튼클릭수 == 1) {
+                    axios
+                      .get("https://codingapple1.github.io/shop/data3.json")
+                      .then((결과) => {
+                        let copy = [...shoes, ...결과.data];
+                        console.log(copy);
+                        슈즈변경(copy);
+                        setTimeout(() => {
+                          로딩상태변경(false);
+                        }, 500);
+                      })
+                      .catch(() => {
+                        console.log("버튼클릭 2차 실패함ㅅㄱ");
+                        setTimeout(() => {
+                          로딩상태변경(false);
+                        }, 500);
+                      });
+                  } else {
+                    console.log("상품 더 없음");
+                    setTimeout(() => {
+                      로딩상태변경(false);
+                    }, 500);
+                  }
+                }}
+              >
+                {버튼클릭수 < 3 ? <span>더보기임</span> : <span>끝임ㅅㄱ</span>}
+              </button>
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/detail/:id"
+          element={
+            <Context1.Provider value={{ 재고, shoes }}>
+              <Detail shoes={shoes} />
+            </Context1.Provider>
+          }
+        />
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>멤버임</div>}></Route>
           <Route path="location" element={<div>위치정보임</div>}></Route>
@@ -59,12 +133,6 @@ function App() {
         <Route path="/about/location" element={<About />} />
         <Route path="*" element={<div>없는페이지요</div>}></Route>
       </Routes>
-      {
-        로딩상태 == true
-        ? <div className="로딩중">로딩중</div>
-        : null
-      }
-
     </div>
   );
 }
@@ -82,57 +150,6 @@ function About() {
     <div>
       <h4>회사정보임</h4>
       <Outlet></Outlet>
-    </div>
-  );
-}
-function Main(props) {
-  return (
-    <div>
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          {props.shoes.map((a, i) => {
-            return <Card i={i} shoes={a}></Card>;
-          })}
-        </Row>
-      </Container>
-      <button
-        onClick={() => {
-          버튼클릭수변경(버튼클릭수 + 1); 
-          if (버튼클릭수 == 1) {
-            axios
-              .get("https://codingapple1.github.io/shop/data2.json")
-              .then((결과) => {
-                let copy = [...shoes, ...결과.data];
-                console.log(copy);
-                슈즈변경(copy);
-                로딩상태변경(false)
-              })
-              .catch(() => {
-                console.log("버튼클릭 1차 실패함ㅅㄱ");
-                로딩상태변경(false)
-              });
-          } else if (버튼클릭수 == 2) {
-            axios
-              .get("https://codingapple1.github.io/shop/data3.json")
-              .then((결과) => {
-                let copy = [...shoes, ...결과.data];
-                console.log(copy);
-                슈즈변경(copy);
-                로딩상태변경(false)
-              })
-              .catch(() => {
-                console.log("버튼클릭 2차 실패함ㅅㄱ");
-                로딩상태변경(false)
-              });
-          } else {
-            console.log("상품 더 없음");
-            로딩상태변경(false)
-          }
-        }}
-      >
-        아무버튼
-      </button>
     </div>
   );
 }
--- sample4/src/pages/Detail.js
+++ sample4/src/pages/Detail.js
@@ -1,34 +1,42 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Nav } from "react-bootstrap";
 import "./Detail.css";
+import { Context1 } from "./../App.js";
 
 function Detail(props) {
+  let { 재고, shoes } = useContext(Context1);
   let { id } = useParams();
   let [count, setCount] = useState(0);
   let [discount, setDiscount] = useState(true);
   let [숫자경고, 숫자경고변경] = useState(false);
   let [입력값, 입력값변경] = useState("");
   let [탭, 탭변경] = useState(2);
+  let [디테일페이드, 디테일페이드변경] = useState("");
 
+  useEffect(() => {
+    let b = setTimeout(() => 디테일페이드변경("end"), 100);
+    return () => {
+      clearTimeout(b);
+      디테일페이드변경("");
+    };
+  }, []);
   useEffect(() => {
     let a = setTimeout(() => {
       setDiscount(false);
     }, 2000);
-
     if (isNaN(입력값)) {
       숫자경고변경(true);
     } else {
       숫자경고변경(false);
     }
     return () => {
-      console.log(1);
       clearTimeout(a);
     };
-  });
+  }, [입력값]);
   const filteredShoes = props.shoes.filter((shoe) => shoe.id == id);
   return (
-    <div className="container">
+    <div className={`container start ${디테일페이드}`}>
       {discount == true ? (
         <div className="alert alert-warning">2초 이내 구매시 할인</div>
       ) : null}
@@ -54,7 +62,6 @@ function Detail(props) {
               입력값변경(e.target.value);
             }}
           />
-
           <div></div>
           <div className="col-md-6">
             <h4 className="pt-5">찾은 상품 : {filteredShoes[0].title}</h4>
@@ -65,16 +72,37 @@ function Detail(props) {
           </div>
           <Nav variant="tabs" defaultActiveKey="link1">
             <Nav.Item>
-              <Nav.Link eventKey="link0" onClick={()=>{탭변경(0)}}>버튼0</Nav.Link>
+              <Nav.Link
+                eventKey="link0"
+                onClick={() => {
+                  탭변경(0);
+                }}
+              >
+                버튼0
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link1" onClick={()=>{탭변경(1)}}>버튼1</Nav.Link>
+              <Nav.Link
+                eventKey="link1"
+                onClick={() => {
+                  탭변경(1);
+                }}
+              >
+                버튼1
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link2" onClick={()=>{탭변경(2)}}>버튼2</Nav.Link>
+              <Nav.Link
+                eventKey="link2"
+                onClick={() => {
+                  탭변경(2);
+                }}
+              >
+                버튼2
+              </Nav.Link>
             </Nav.Item>
           </Nav>
-          <TapContent 탭={탭}></TapContent>
+          <TapContent shoes={props.shoes} 탭={탭}></TapContent>
         </div>
       ) : (
         <div>페이지가 이상해요</div>
@@ -83,15 +111,28 @@ function Detail(props) {
   );
 }
 
-function TapContent(props) {
-  if (props.탭 == 0) {
-    return <div>내용0</div>;
-  }
-  if (props.탭 == 1) {
-    return <div>내용1</div>;
-  }
-  if (props.탭 == 2) {
-    return <div>내용2</div>;
-  }
+function TapContent({ 탭, shoes }) {
+  let [fade, setFade] = useState("");
+  let { 재고 } = useContext(Context1);
+  useEffect(() => {
+    //fade라는 state를 end로 바꿔주세요~
+    let a = setTimeout(() => {
+      setFade("end");
+    }, 100); // 타이머이므로 클린업하고싶으면 변수에 할당하고
+    return () => {
+      clearTimeout(a); // 타이머 삭제도 됨
+      setFade("");
+    }; // clean up function으로 뗏다 붙여주기
+  }, [탭]);
+  return (
+    <div className={`start ${fade} 빈칸`}>
+      {재고}
+      {
+        [<div>내용0{shoes[0].title}</div>, <div>내용1</div>, <div>내용2</div>][
+          탭
+        ]
+      }
+    </div>
+  );
 }
 export default Detail;
